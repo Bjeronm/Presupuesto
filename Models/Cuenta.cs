ea48a88270b16462e3e0a292f5746c0d9d261c8e
Jeronm <you@example.com>
2022-10-19 22:14:50 -0600

    initial

---
+++ Models/Cuenta.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Nombre { get; set; }
+        [Display(Name ="Tipos de Cuenta")]
+        public int TipoCuentaId { get; set; }
+        public decimal Balance { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo nombre es requerido")]
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
